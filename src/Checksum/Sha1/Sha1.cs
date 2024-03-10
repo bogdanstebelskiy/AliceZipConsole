19efae53236422fdfd4174c31197e67c812d6857
bogdanstebelskiy <bogdanstebelskiy@gmail.com>
2024-03-10 17:10:44 +0200

    Added Sha1 checksum support

--- src/Checksum/Sha1/Sha1.cs
+++ src/Checksum/Sha1/Sha1.cs
@@ -5,14 +5,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Security.Cryptography;
 
-namespace ConsoleApp.src.Checksum.Sha256
+namespace ConsoleApp.src.Checksum.Sha1
 {
-    internal class Sha256 : IChecksum
+    internal class Sha1 : IChecksum
     {
         public string CalculateChecksum(string inPath, int bufferSize = 1024 * 50)
         {
-            using var sha256 = System.Security.Cryptography.SHA256.Create();
-            
+            using var sha1 = System.Security.Cryptography.SHA1.Create();
+
             var attributes = File.GetAttributes(inPath);
             var buffer = new byte[bufferSize];
 
@@ -21,14 +21,14 @@ namespace ConsoleApp.src.Checksum.Sha256
             var bytesRead = 0;
             while ((bytesRead = fileStream.Read(buffer, 0, bufferSize)) != 0)
             {
-                sha256.TransformBlock(buffer, 0, bytesRead, buffer, 0);
+                sha1.TransformBlock(buffer, 0, bytesRead, buffer, 0);
             }
 
-            sha256.TransformFinalBlock(buffer, 0, 0);
-            
-            if (sha256.Hash != null)
+            sha1.TransformFinalBlock(buffer, 0, 0);
+
+            if (sha1.Hash != null)
             {
-                return BitConverter.ToString(sha256.Hash).Replace("-", "").ToLower();
+                return BitConverter.ToString(sha1.Hash).Replace("-", "").ToLower();
             }
 
             return "";
